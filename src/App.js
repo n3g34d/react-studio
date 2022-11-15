53492e2df3fd2593bebaf52a043ab2ea930dc0d9
Grace Dorantes <115292948+n3g34d@users.noreply.github.com>
2022-11-15 13:39:39 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from './components/BakeryItem.js';
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,21 +12,32 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
 
   return (
-    <div className="App">
+    <div className="App" style={{
+      display: 'flex',
+      width: '70%'
+    }}>
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem  description={item.description} cart = {cart} setCartTotal = {setCartTotal} key={index}  item={item} name={item.name} price={item.price}/>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <ul>
+        {cart.map(i => (<p>{i}</p>))}
+        <p>Total: ${cartTotal}</p>
+        </ul>
+        
+
       </div>
     </div>
   );
 }
+
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,37 @@
 // TODO: create a component that displays a single bakery item
+import { useState } from 'react';
+
+export default function BakeryItem(props) {
+    const item = props.item; 
+    const name = props.name; 
+    const price = props.price; 
+
+    const onClick = () => {
+        {props.setCartTotal((count) => count + price)}
+        {props.cart.push([name])}
+    }
+
+    return (
+        
+        <div
+        style={{
+            width: 'auto',
+            border: 'solid 2px black',
+
+        }}>
+            <img src={item.image} style={{ width: '100%' }}></ img>
+            <h1>{name}</h1>
+            <p>{item.description}</p>
+            <h2>${price}</h2>
+            <button  style={{
+                    width: 100,
+                    height: 50,
+                    justifyContent: 'center',
+                    backgroundColor: 'lightyellow',
+                    marginBottom: 10
+                }}
+                onClick = {onClick} class = "button"> ADD TO CART </button>
+            
+        </div>
+    )
+}
